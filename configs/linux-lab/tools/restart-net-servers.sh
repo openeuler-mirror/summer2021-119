da9d7f1fd673a63868813bfce2fac7fb4e3844bc
Riumin <shaunleonotryusho@outlook.com>
2021-08-09 16:25:56 +0800

    oel-headless-alpha-1.2

--- configs/linux-lab/tools/restart-net-servers.sh
+++ configs/linux-lab/tools/restart-net-servers.sh
@@ -5,8 +5,11 @@
 # Copyright (C) 2016-2021 Wu Zhangjin <falcon@ruma.tech>
 #
 
-service tftpd-hpa restart
+# service tftpd-hpa restart
+systemctl restart tftp-server
 sleep 1
-service rpcbind restart
+# service rpcbind restart
+systemctl restart rpcbind
 sleep 1
-service nfs-kernel-server restart
+# service nfs-kernel-server restart
+systemctl restart nfs-server
